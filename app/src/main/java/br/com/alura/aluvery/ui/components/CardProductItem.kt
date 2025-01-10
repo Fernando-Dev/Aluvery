17677ba53ed09f9ff9e650075a9672bfc3c6620d
˜Fernando <fernando_dev@outlook.com.br>
2025-01-10 16:47:18 -0300

    feat: adicionar alteracao

--- app/src/main/java/br/com/alura/aluvery/ui/components/CardProductItem.kt
+++ app/src/main/java/br/com/alura/aluvery/ui/components/CardProductItem.kt
@@ -1,6 +1,7 @@
 package br.com.alura.aluvery.ui.components
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -11,6 +12,10 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.layout.ContentScale
@@ -30,13 +35,18 @@ import java.math.BigDecimal
 fun CardProductItem(
     product: Product,
     modifier: Modifier = Modifier,
-    elevation: Dp = 4.dp
+    elevation: Dp = 4.dp,
+    isExpanded: Boolean = false
 ) {
+    var expanded by remember { mutableStateOf(isExpanded) }
     Card(
         modifier
             .fillMaxWidth()
             .heightIn(150.dp)
             .shadow(elevation)
+            .clickable {
+                expanded = !expanded
+            }
     ) {
         Column {
             AsyncImage(
@@ -61,12 +71,15 @@ fun CardProductItem(
                     text = product.price.toBrazilianCurrency()
                 )
             }
-            product.description?.also {
-                Text(
-                    text = product.description,
-                    Modifier
-                        .padding(16.dp)
-                )
+
+            if (expanded) {
+                product.description?.also {
+                    Text(
+                        text = product.description,
+                        Modifier
+                            .padding(16.dp)
+                    )
+                }
             }
         }
     }
@@ -96,8 +109,9 @@ private fun CardProductItemWithDescriptionPreview() {
                 product = Product(
                     name = "Teste",
                     price = BigDecimal("9.99"),
-                    description = LoremIpsum(50).values.first()
-                )
+                    description = LoremIpsum(50).values.first(),
+                ),
+                isExpanded = true
             )
         }
     }
